6b52d4c906b96d2d83daba79b3e6e6c3a372f61f
Sabelli <sabellinho@gmail.com>
2026-09-11 20:27:02 -0300

    chore: atualiza Migrations correção FeedbackNPS

---
+++ Arkive_API/Infrastructure/Data/Migrations/20260911232620_CorrecaoFeedbackNPS.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arkive_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoFeedbackNPS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
